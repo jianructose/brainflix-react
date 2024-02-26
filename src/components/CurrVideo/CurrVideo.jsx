cf2dbdc5e4be5e0ba9f71ccb90d152c918bd3796
jianructose <70822647+jianructose@users.noreply.github.com>
2024-02-26 03:24:59 +0000

    style the curr video info

--- src/components/CurrVideo/CurrVideo.jsx
+++ src/components/CurrVideo/CurrVideo.jsx
@@ -1,32 +1,39 @@
 import "./CurrVideo.scss";
 import viewsIcon from "../../assets/icons/views.svg";
 import likesIcon from "../../assets/icons/likes.svg";
+import { formatDistanceToNow } from "date-fns";
 
 function CurrVideo(props) {
   return (
     <section className="curr-video">
       <h1 className="curr-video__title">{props.currVideo.title}</h1>
-      <div className="divider"></div>
+
+      <div className="curr-video__divider"></div>
+
       {/* container of info and stats */}
       <div className="curr-video__info-stats-wrapper">
         <div className="curr-video__info">
-          <p className="curr-video__channel">{props.currVideo.channel}</p>
+          <p className="curr-video__channel">By {props.currVideo.channel}</p>
+
           {/* render timestamp */}
           <p className="curr-video__date">
-            {new Date(props.currVideo.timestamp).toLocaleDateString("en-US", {
-              timeZone: "America/Los_Angeles",
-            })}
+            {formatDistanceToNow(new Date(props.currVideo.timestamp), {
+              addSuffix: true,
+            })}{" "}
           </p>
         </div>
-        <div class-name="curr-video__stats">
+
+        <div className="curr-video__stats">
           <div className="curr-video__views">
             <img
               src={viewsIcon}
               alt="Views icon"
               className="curr-video__views-icon"
             />
+
             <p className="curr-video__views-count">{props.currVideo.views}</p>
           </div>
+
           <div className="curr-video__likes">
             <img
               src={likesIcon}
@@ -37,7 +44,9 @@ function CurrVideo(props) {
           </div>
         </div>
       </div>
+
       <div className="divider"></div>
+
       <p className="curr-video__description">{props.currVideo.description}</p>
     </section>
   );
