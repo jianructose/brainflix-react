890037f5b93ed20e001c592eb2ca62540b255df0
jianructose <70822647+jianructose@users.noreply.github.com>
2024-02-26 01:40:57 +0000

    add hero vid player style

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,15 +1,24 @@
-import videoData from "../../data/videos.json";
 import "./Hero.scss";
 
+// Define the API key
+const apiKey = "87db0f55-c275-4878-a7ad-6eb00d9a5c17";
+
 function Hero(props) {
+  const videoSrc = `${props.currVideo.video}?api_key=${apiKey}`;
+
   return (
     <div className="hero">
       <video
         className="hero__video"
         poster={props.currVideo.image}
         controls
-      ></video>
-      <source src={props.currVideo.video} type="video/mp4" />
+        muted
+        loop
+        buffer
+        scrubber
+      >
+        <source src={videoSrc} type="video/mp4" />{" "}
+      </video>
     </div>
   );
 }
