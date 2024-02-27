8195c442764647ad652e260bc2db2aaabf961570
Jianru D <dengjr1509@gmail.com>
2024-02-27 00:17:16 -0800

    refactor homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,63 @@
 import "./App.scss";
 import Header from "./components/Header/Header";
-import Hero from "./components/Hero/Hero";
-import CurrVideo from "./components/CurrVideo/CurrVideo";
-import Comments from "./components/Comments/Comments";
-import SideVideos from "./components/SideVideos/SideVideos";
-import { useState } from "react";
-import videoData from "./data/video-details.json";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+// import Hero from "./components/Hero/Hero";
+// import CurrVideo from "./components/CurrVideo/CurrVideo";
+// import Comments from "./components/Comments/Comments";
+// import SideVideos from "./components/SideVideos/SideVideos";
+// import { useState } from "react";
+// import videoData from "./data/video-details.json";
 
 function App() {
-  const [currVideo, setCurrVideo] = useState(videoData[0]);
-
-  const [videoList, setVideoList] = useState(videoData);
-  console.log("videoList", videoList);
-
-  function handleVideoClick(id) {
-    const newCurrVideo = videoList.find((video) => video.id === id);
-    setCurrVideo(newCurrVideo);
-  }
-
   return (
-    <main className="App">
-      {/* header component */}
+    <Router>
       <Header />
-
-      {/* hero component */}
-      <Hero currVideo={currVideo} />
-
-      <div className="main-info">
-        <div className="main-info__curr-video">
-          {/* main video info component */}
-          <CurrVideo currVideo={currVideo} />
-
-          {/* comments component */}
-          <Comments currVideo={currVideo} />
-        </div>
-        <div className="main-info__side-videos">
-          {/* video list component */}
-          <SideVideos
-            sideVideos={videoList}
-            currVideo={currVideo}
-            handleVideoClick={handleVideoClick}
-          />
-        </div>
-      </div>
-    </main>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
   );
 }
+
+// function App() {
+//   const [currVideo, setCurrVideo] = useState(videoData[0]);
+
+//   const [videoList, setVideoList] = useState(videoData);
+//   console.log("videoList", videoList);
+
+//   function handleVideoClick(id) {
+//     const newCurrVideo = videoList.find((video) => video.id === id);
+//     setCurrVideo(newCurrVideo);
+//   }
+
+//   return (
+//     <main className="App">
+//       {/* header component */}
+//       <Header />
+
+//       {/* hero component */}
+//       <Hero currVideo={currVideo} />
+
+//       <div className="main-info">
+//         <div className="main-info__curr-video">
+//           {/* main video info component */}
+//           <CurrVideo currVideo={currVideo} />
+
+//           {/* comments component */}
+//           <Comments currVideo={currVideo} />
+//         </div>
+//         <div className="main-info__side-videos">
+//           {/* video list component */}
+//           <SideVideos
+//             sideVideos={videoList}
+//             currVideo={currVideo}
+//             handleVideoClick={handleVideoClick}
+//           />
+//         </div>
+//       </div>
+//     </main>
+//   );
+// }
 
 export default App;
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,50 @@
+import "./HomePage.scss";
+import Hero from "../../components/Hero/Hero";
+import CurrVideo from "../../components/CurrVideo/CurrVideo";
+import Comments from "../../components/Comments/Comments";
+import SideVideos from "../../components/SideVideos/SideVideos";
+import { useState } from "react";
+import videoData from "../../data/video-details.json";
+import { useParams, NavLink } from "react-router-dom";
+
+function HomePage() {
+  const [currVideo, setCurrVideo] = useState(videoData[0]);
+
+  const [videoList, setVideoList] = useState(videoData);
+  console.log("videoList", videoList);
+
+  function handleVideoClick(id) {
+    const newCurrVideo = videoList.find((video) => video.id === id);
+    setCurrVideo(newCurrVideo);
+  }
+
+  return (
+    <main className="App">
+      {/* header component */}
+      {/* <Header /> */}
+
+      {/* hero component */}
+      <Hero currVideo={currVideo} />
+
+      <div className="main-info">
+        <div className="main-info__curr-video">
+          {/* main video info component */}
+          <CurrVideo currVideo={currVideo} />
+
+          {/* comments component */}
+          <Comments currVideo={currVideo} />
+        </div>
+        <div className="main-info__side-videos">
+          {/* video list component */}
+          <SideVideos
+            sideVideos={videoList}
+            currVideo={currVideo}
+            handleVideoClick={handleVideoClick}
+          />
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default HomePage;
