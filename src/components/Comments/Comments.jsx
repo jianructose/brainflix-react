26fa6a0549384ae39ec3e225d7f4c78e79aa5ae2
Jianru D <dengjr1509@gmail.com>
2024-03-03 20:26:30 -0800

    post comment code

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,33 +1,86 @@
 import "./Comments.scss";
 import userAvatar from "../../assets/images/Mohan-muruge.jpg";
-import { formatDistanceToNow } from "date-fns";
+import likeIcon from "../../assets/icons/likes.svg";
+import deleteIcon from "../../assets/icons/delete.svg";
+import { formatDistanceToNow, set } from "date-fns";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
 function Comments({ currVideo }) {
-  const url = `https://project-2-api.herokuapp.com`;
+  const url = `https://unit-3-project-api-0a5620414506.herokuapp.com`;
   // Define the API key
   const apiKey = "87db0f55-c275-4878-a7ad-6eb00d9a5c17";
 
   const [comments, setComments] = useState([]);
+  const [commentPosted, setCommentPosted] = useState(false);
 
   // async function to fetch comments
   const fetchComments = async () => {
     try {
-      const response = await axios.get(
+      const { data } = await axios.get(
         `${url}/videos/${currVideo.id}?api_key=${apiKey}`
-      ); // https://project-2-api.herokuapp.com/videos/:videoId/comments
+      );
 
       // set the initial comments list as the response data
-      setComments(response.data.comments);
+      setComments(data.comments);
     } catch (error) {
       console.log("There was an error fetching comments", error);
     }
   };
 
+  // handle comment submit
+  const handleCommentSubmit = async (e) => {
+    e.preventDefault();
+    const comment = {
+      name: "Mohan Muruge",
+      comment: e.target.comment.value,
+    };
+
+    try {
+      await axios.post(
+        `${url}/videos/${currVideo.id}/comments?api_key=${apiKey}`,
+        comment
+      );
+      console.log("Comment has been posted successfully");
+      setCommentPosted(true);
+    } catch (error) {
+      console.log("There was an error posting the comment", error);
+    }
+
+    e.target.comment.value = "";
+  };
+
+  // asyc function to delete a comment
+  const deleteComment = async (commentId) => {
+    try {
+      await axios.delete(
+        `${url}/videos/${currVideo.id}/comments/${commentId}?api_key=${apiKey}`
+      );
+      console.log(`Comment from ${commentId} has been deleted successfully`);
+
+      // refetch comments
+      fetchComments();
+
+      console.log("comments after delete", comments);
+
+      // setComments(comments.filter((comment) => comment.id !== commentId));
+    } catch (error) {
+      console.log("There was an error deleting the comment", error);
+    }
+  };
+
+  // useEffect to fetch comments every time the currVideo comments list changes
   useEffect(() => {
     fetchComments();
   }, []);
+
+  // useEffect to refetch comments after a comment has been posted
+  useEffect(() => {
+    if (commentPosted) {
+      fetchComments();
+      setCommentPosted(false);
+    }
+  }, [commentPosted]);
 
   return (
     // comments section
@@ -38,7 +91,7 @@ function Comments({ currVideo }) {
         {currVideo.comments.length > 1 ? "s" : ""}
       </h4>
 
-      <form className="form">
+      <form className="form" onSubmit={handleCommentSubmit}>
         {/* form avatar */}
         <img src={userAvatar} alt="user avatar" className="form__avatar" />
 
@@ -52,10 +105,20 @@ function Comments({ currVideo }) {
             <textarea
               className="form__input"
               placeholder="Add a new comment"
+              name="comment"
+              type="text"
+              id="comment"
             ></textarea>
 
             {/* form button */}
-            <button className="form__button">COMMENT</button>
+            <button
+              className="form__button"
+              type="submit"
+              id="submit"
+              name="submit"
+            >
+              COMMENT
+            </button>
           </div>
         </div>
       </form>
@@ -82,7 +145,17 @@ function Comments({ currVideo }) {
                 </div>
                 {/* comment text*/}
                 <p className="comments__text">{comment.comment}</p>
+                <div className="comments__buttons">
+                  <img src={likeIcon} alt="like icon" />
+                  <p className="comments__like-count">{comment.likes}</p>
+                  <img
+                    src={deleteIcon}
+                    alt="delete icon"
+                    // onClick={() => deleteComment(comment.id)}
+                  />
+                </div>
               </div>
+              {/* add like and delete button with icon */}
             </li>
             <div className="divider"></div>
           </ul>
